9cc9b241e41e5d78078496572a2aaae2491a1e1e
Martín Acosta <tinchoz49@gmail.com>
2016-02-12 01:38:37 -0300

    route.mode hash in example

--- example/index.js
+++ example/index.js
@@ -3,6 +3,8 @@ import PageOne from './components/page-one.js';
 import PageTwo from './components/page-two.js';
 import PageThree from './components/page-three.js';
 
+m.route.mode = 'hash';
+
 m.route(document.body, '/pageOne', {
     '/pageOne': PageOne,
     '/pageTwo': PageTwo,
